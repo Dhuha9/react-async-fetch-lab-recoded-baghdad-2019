d7117dada93a88c8ee75cc56808d978512d83f49
Dhuha Hasan <duha_9@yahoo.com>
2019-12-06 07:10:00 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
 // create your App component here
+import React from 'react'
+
+export default class App from React.component{
+  componentDidMount(){
+  return  fetch("http://api.open-notify.org/astros.json").then(res=>res.json()).then(json=>json)
+  }
+}
